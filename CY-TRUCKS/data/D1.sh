7d2d60a948803cfa8488e8c09dd76dfd952da6ee
ArnoGrl <153304352+ArnoGrl@users.noreply.github.com>
2023-12-28 12:27:22 +0100

    D1.sh modified
    j'ai modifié D1 dans data, mais c'est encore pas ouf

--- CY-TRUCKS/data/D1.sh
+++ CY-TRUCKS/data/D1.sh
@@ -19,22 +19,25 @@ file=$1
 output_file="conducteur_counts.txt"
 
 # Compter les trajets par conducteur
-time (awk -F';' '{print $6}' "$file" | sort | uniq -c | sort -rn > "$output_file" && echo "La commande a pris $(date +%s.%N -d@$SECONDS) secondes")
+time (awk -F';' '{print $6}' "$file" | sort | uniq -c | sort -rn | head -10 > "$output_file" && echo "La commande a pris $(date +%s.%N -d@$SECONDS) secondes")
 echo -e "\nliste des 10 plus gros conducteur : \n"
 
-cat $output_file | head -10
+cat conducteur_counts.txt
 
 
 # Utilisation des données dans le fichier temporaire pour générer le graphique Gnuplot
 gnuplot << EOF
 set terminal pngcairo enhanced font "arial,10" size 800,600
-set output 'conducteur_counts.png'
+set output 'conducteur_counts_horizontal.png'
 set title 'Nombre de trajets par conducteur'
-set xlabel 'Nombre de trajets'
 set ylabel 'Conducteur'
+set xlabel 'Nombre de trajets'
+set style data histogram
+set style fill solid border -1
 set ytics nomirror
 set yrange [0:*]
-set style data histograms
-set style fill solid border -1
-plot '$output_file' using 1:xtic(2) title ''
+set xrange [0:*]
+set boxwidth 0.5 relative
+set key off
+plot '$output_file' using 1:xtic(2) with boxes title ''
 EOF
